d2877ff7b08a04696412deab8a8fc2914a60b33f
anyongjin <anyongjin163@163.com>
2025-06-09 18:15:00 +0800

    update

--- python/set_gopy_env.py
+++ python/set_gopy_env.py
@@ -9,9 +9,9 @@ gopy_libdir = os.path.join(sys.prefix, 'libs')
 
 with open("gopy_env.bat", "w") as f:
     f.write(f'@echo off\n')
-    f.write(f'set "GOPY_PYLIB={gopy_pylib}"\n')
-    f.write(f'set "GOPY_LIBDIR={gopy_libdir}"\n')
-    f.write(f'setx "GOPY_PYLIB={gopy_pylib}"\n')
-    f.write(f'setx "GOPY_LIBDIR={gopy_libdir}"\n')
+    f.write(f'set GOPY_PYLIB "{gopy_pylib}"\n')
+    f.write(f'set GOPY_LIBDIR "{gopy_libdir}"\n')
+    f.write(f'setx GOPY_PYLIB "{gopy_pylib}"\n')
+    f.write(f'setx GOPY_LIBDIR "{gopy_libdir}"\n')
     f.write(f'echo GOPY_PYLIB set to %GOPY_PYLIB%\n')
     f.write(f'echo GOPY_LIBDIR set to %GOPY_LIBDIR%\n') 
